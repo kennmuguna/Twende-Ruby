7167a574df6de26e384b4709931229d31f5771e6
LatashaWanjira <latasha.wanjira@gmail.com>
2017-03-08 10:02:57 +0300

    linking of all the pages to the overlay

--- app.rb
+++ app.rb
@@ -5,38 +5,10 @@ get('/') do
   erb(:index)
 end
 
-get('/about') do
-  erb(:about)
-end
-
-get('/routes') do
-  erb(:routes)
-end
-
-get('/payment') do
-  erb(:payment)
-end
-
 get('/FAQ') do
   erb(:FAQ)
 end
 
-get('/terms_and_conditions') do
-  erb(:terms_and_conditions)
-end
-
-get('/driver') do
-  erb(:driver)
-end
-
-get('/driver/new') do
-  erb(:driver_form)
-end
-
-get('/passenger') do
-  erb(:passenger)
-end
-
-get('/passengers/new') do
-  erb(:passenger_form)
+get('/about') do
+  erb(:about)
 end
