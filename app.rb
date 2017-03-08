6eb1d5a8758f0be685b89a37d48c5db508c20b6b
kennmuguna <kennmuguna@gmail.com>
2017-03-08 18:34:11 +0300

    database referencing works

--- app.rb
+++ app.rb
@@ -14,6 +14,14 @@ get('/FAQ') do
   erb(:FAQ)
 end
 
+get('/payment') do
+  erb(:payment)
+end
+
+get('/terms_and_conditions') do
+  erb(:terms_and_conditions)
+end
+
 get('/about') do
   erb(:about)
 end
@@ -22,30 +30,54 @@ get("/driver_signin") do
   erb(:driver_form)
 end
 
-get("/passenger_signin")  do
+get("/passenger_signin/:id")  do
+  @driver = Driver.find(params.fetch("id").to_i())
   erb(:passenger_form)
 end
 
 get ("/driver") do
-  @drivers = Driver.all()
+  @driver
   erb(:driver)
 end
 
-post("/driver_form")  do
+post("/driver")  do
   name = params.fetch("name").capitalize!()
   email = params.fetch("email")
   phone_number = params.fetch("phone_number").to_i()
   car = params.fetch("car").capitalize!()
   number_plate = params.fetch("number_plate")
   space = params.fetch("space").to_i()
-  route_from = params.fetch("route_from").capitalize!()
-  route_to = params.fetch("route_to").capitalize!()
+  route_from = params.fetch("route_from")
+  route_to = params.fetch("route_to")
   time = params.fetch("time")
-  driver = Driver.new({:name => name, :email => email, :phone_number => phone_number, :car => car, :number_plate => number_plate, :space => space, :route_from => route_from, :route_to => route_to, :time => time, :id => nil})
-  driver.save()
-  erb(:driver)
+  @driver = Driver.new({:name => name, :email => email, :phone_number => phone_number, :car => car, :number_plate => number_plate, :space => space, :route_from => route_from, :route_to => route_to, :time => time, :id => nil})
+  if @driver.save()
+    erb(:driver)
+  else
+    erb(:errors1)
+  end
+end
+
+post("/passenger")  do
+  name = params.fetch("name").capitalize!()
+  email = params.fetch("email")
+  phone_number =  params.fetch("phone_number").to_i()
+  driver_id = params.fetch("driver_id").to_i()
+  @driver = Driver.find(driver_id)
+  @passenger = Passenger.new({:name => name, :email => email, :phone_number => phone_number, :driver_id => driver_id})
+  if @passenger.save()
+    erb(:passenger)
+  else
+    erb(:errors)
+  end
 end
 
 get("/passenger")  do
+  @passenger
   erb(:passenger)
+end
+
+get("/routes") do
+  @drivers = Driver.all()
+  erb(:routes)
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,5 +1,8 @@
 require("spec_helper")
 
 describe(Passenger) do
-
+  it("validates presence of fields")  do
+    passenger = Passenger.new({:name => "", :email  => "", :phone_number => ""})
+    expect(passenger.save()).to(eq(false))
+  end
 end
